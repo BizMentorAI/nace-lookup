5f000e63916b8f4b4f467b2458f72483dfd1c5f6
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-05-16 15:34:20 -0600

    WIP

--- src/workers/autocomplete/autocomplete.cljs
+++ src/workers/autocomplete/autocomplete.cljs
@@ -1,19 +1,23 @@
 (ns workers.autocomplete.autocomplete
   (:require-macros [macros :refer [inline-resource]]))
 
-(defn ^:async fetch-data []
-  ; TODO: The JSON path should come from a variable (tangling).
-  (let [response (js/await (js/fetch "/workers/autocomplete/data.json"))
-        body (js/await (.json response))] body))
-
 ; Does inlining work?
 (def r (inline-resource "/Users/jakub/Documents/Projects/bizmentor/nace-finder-modern/README.org"))
 (js/console.log r)
 
+
+
+(defn ^:async fetch-data []
+                                        ; TODO: The JSON path should come from a variable (tangling).
+  (let [response (js/await (js/fetch "/workers/autocomplete/data.json"))
+        body (js/await (.json response))] body))
+
 (def data (js->clj (js/await (fetch-data)) {:keywordize-keys true}))
 
 (defn- filter-items [data search-term]
-  (persistent! (into [] (partial data-transducer search-term) data)))
+  ;(persistent! (into [] (partial data-transducer search-term) data))
+  []
+  )
 
 (defn handle-message [event]
   (let [search-term event.data]
