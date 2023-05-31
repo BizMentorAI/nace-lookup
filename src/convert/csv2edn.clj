d0250a2e363a660d37f89ce8ede747ca1cf23ef0
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-05-31 10:44:54 -0600

    Use maps rather than vectors

--- src/convert/csv2edn.clj
+++ src/convert/csv2edn.clj
@@ -1,30 +1,27 @@
 #!/usr/bin/env bb
 
-; csv2edn.clj src/data/cpa.csv 1 > src/data/cpa.edn
-; csv2edn.clj src/data/cpc.csv 0 > src/data/cpc.edn
-; csv2edn.clj src/data/cpa2cpc.csv 1,4 > src/data/cpa2cpc.edn
+(defn to-int [str]
+  (Integer/parseInt str))
 
-; TODO: write the header / maybe convert to a map.
-(defn exit-with-error [message]
-  (throw (ex-info
-          (str \u001b "[31m" "Error: " \u001b "[0m" message)
-          {:babashka/exit 1})))
+(defn keywordise [str]
+  (keyword (-> (str/lower-case str)
+               (str/replace "." "")
+               (str/replace " " "-"))))
 
-(when-not (= (count *command-line-args*) 2)
-  (exit-with-error "2 arguments (input file path and numeric colum index) expected."))
+(defn csv-data->maps [csv-data]
+  (map zipmap
+       (->> (first csv-data) ;; header
+            (map keywordise)
+            repeat)
+	  (rest csv-data)))
 
-(def input-file-path (first *command-line-args*))
+(defn process [path hook]
+  (let [out-path (str/replace path #".csv$" ".edn")]
+    (with-open [reader (io/reader path)]
+      (println (str "~ Writing " out-path "."))
+      (let [results (csv-data->maps (csv/read-csv reader))]
+        (spit out-path (prn-str (vec results)))))))
 
-(def num-col-ind
-  (map #(Integer/parseInt %)
-       (str/split (last *command-line-args*) #",")))
-
-(with-open [reader (io/reader input-file-path)]
-  (print "[")
-  (doseq [i (drop 1 (csv/read-csv reader))]
-    (print (str " " (prn-str
-                     (reduce (fn [acc ind]
-                               (update-in acc [ind] #(Integer/parseInt %)))
-                             i
-                             num-col-ind)))))
-  (println "]"))
+(process "src/data/cpa.csv" #(update-in % [1] to-int))
+(process "src/data/cpc.csv" #(update-in % [0] to-int))
+(process "src/data/cpa2cpc.csv" #(update-in % [1 4] to-int))
