394255f817f7fa38539ce130367c617250e57616
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-06-13 12:01:24 -0600

    HS JSON -> EDN

---
+++ src/convert/json2edn.clj
@@ -0,0 +1,19 @@
+#!/usr/bin/env bb
+
+(require '[clojure.pprint :refer [pprint]])
+(require '[clojure.set :as set])
+
+(defn keywordise [str]
+  (keyword (-> (str/lower-case str)
+               (str/replace "." "")
+               (str/replace " " "-"))))
+
+(defn process [path hook]
+  (let [out-path (str/replace path #".json$" ".edn")]
+    (println (str "~ Writing " out-path "."))
+    (let [record (json/parse-string (slurp path) true)
+          _ (prn record)
+          result (hook record)]
+      (spit out-path (with-out-str (pprint result))))))
+
+(process "src/data/hs2022.json" #(:results %))
