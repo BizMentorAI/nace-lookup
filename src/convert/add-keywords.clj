fd1e32de3d95c5ccc4336a3b3b095d6067ba1518
Jakub Šťastný <jakub-stastny@users.noreply.github.com>
2023-07-01 06:57:14 -0600

    WIP

--- src/convert/add-keywords.clj
+++ src/convert/add-keywords.clj
@@ -23,15 +23,18 @@
   (let [message (str "Keywords for " (:code record) " " (:label record))]
     (shell {:out :string :err :string} "git" "commit" data-path "-m" message)))
 
+(defn my-sorted-set [coll]
+  (apply sorted-set-by #(compare (str/lower-case %1) (str/lower-case %2)) coll))
+
 (defn readline [label]
   (print (str label ": ")) (flush)
-  (apply sorted-set (remove empty? (str/split (read-line) #"\s*,\s*"))))
+  (my-sorted-set (remove empty? (str/split (read-line) #"\s*,\s*"))))
 
 (defn edit-record [cursor record]
   (when-not record
     (throw (ex-info "Empty record" {:cursor cursor})))
 
-  (puget/cprint (select-keys record [:label]))
+  (puget/cprint (select-keys record [:code :label]))
   (println)
   (puget/cprint
    (postwalk (fn [i]
@@ -50,14 +53,24 @@
     (reset! records (assoc-in @records (conj cursor :exc) exc))
 
     ; Do in a background thread.
+    ; TODO: Don't exit until results saved.
+    ; Is exc useful at all?
+    ; Review data.edn
+    ; Progress counter: how many items of total are done.
+    ; Are we doing L4 as well?
+    ; Document this. For instance seed in L4 has weight, but less than L6 rel and that is still less than L6 syn.
+    ; Write the algorythm (for the documentation) of how the matching will work.
     (future
       (save-results)
       (commit record))))
 
 (defn post-process [cursor record]
-  (prn record)
-  (reset! records (assoc-in @records (conj cursor :syn) (apply sorted-set (:syn record))))
-  (prn (get-in @records cursor)))
+  (when (not (vector? (get-in @records cursor)))
+    (reset! records (assoc-in @records (conj cursor :syn) (my-sorted-set (:syn record)))))
+  (when (not (vector? (get-in @records cursor)))
+    (reset! records (assoc-in @records (conj cursor :rel) (my-sorted-set (:rel record)))))
+  (when (not (vector? (get-in @records cursor)))
+    (reset! records (assoc-in @records (conj cursor :exc) (my-sorted-set (:exc record))))))
 
 (defn get-cursors [item cursor]
   (cond
