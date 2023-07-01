3ac46e0dafcbbe8950333f8080eeca7ef5ef3041
Jakub Šťastný <60524926+jakub-stastny@users.noreply.github.com>
2023-07-01 06:59:05 -0600

    Adding more keywords (#4)
    * Keywords for 01.11.61 Beans, green
    * Keywords for 01.11.71 Beans, dry
    * Keywords for 01.11.72 Broad beans, dry
    * Keywords for 01.11.73 Chick peas, dry
    * Keywords for 01.11.74 Lentils, dry
    * Keywords for 01.11.75 Peas, dry
    * Keywords for 01.11.76 Cow peas
    * Keywords for 01.11.77 Pigeon peas, dry
    * Keywords for 01.11.79 Pulses (dried leguminous vegetables) n.e.c.
    * Keywords for 01.11.81 Soya beans
    * Keywords for 01.11.82 Groundnuts, in shell
    * Keywords for 01.11.91 Lin seed
    * Keywords for 01.11.92 Mustard seed
    * Keywords for 01.11.93 Rape or colza seed
    * Keywords for 01.11.94 Sesame seed
    * Keywords for 01.11.95 Sunflower seed
    * Keywords for 01.11.96 Castor oil seeds
    * Keywords for 01.12.10 Rice, not husked
    * Keywords for 01.13.11 Asparagus
    * Keywords for 01.13.12 Cabbages
    * Keywords for 01.13.13 Cauliflowers and broccoli
    * Keywords for 01.13.14 Lettuce

--- src/convert/add-keywords.clj
+++ src/convert/add-keywords.clj
@@ -8,26 +8,40 @@
 (require '[clojure.walk :refer [postwalk]])
 (require '[babashka.process :refer [shell]])
 
+; Next step:
+; - Sort alphabetically & filter out possible duplicates in keywords.
+; - Delete extra? Probably yes for this current version.
+; - Convert to JSON.
+
 (def data-path "public/workers/autocomplete/data.edn")
 (def records (atom (edn/read-string (slurp data-path))))
 
 (defn save-results []
-  (pprint @records {:writer (clojure.java.io/writer
-                             "public/workers/autocomplete/data.edn")}))
+  (pprint @records {:writer (clojure.java.io/writer data-path)}))
 
 (defn commit [record]
   (let [message (str "Keywords for " (:code record) " " (:label record))]
     (shell {:out :string :err :string} "git" "commit" data-path "-m" message)))
 
+(defn my-sorted-set [coll]
+  (apply sorted-set-by #(compare (str/lower-case %1) (str/lower-case %2)) coll))
+
 (defn readline [label]
   (print (str label ": ")) (flush)
-  (vec (remove empty? (str/split (read-line) #"\s*,\s*"))))
+  (my-sorted-set (remove empty? (str/split (read-line) #"\s*,\s*"))))
 
 (defn edit-record [cursor record]
   (when-not record
     (throw (ex-info "Empty record" {:cursor cursor})))
 
-  (puget/cprint record)
+  (puget/cprint (select-keys record [:code :label]))
+  (println)
+  (puget/cprint
+   (postwalk (fn [i]
+               (when (not (and (coll? i)
+                               (= (count i) 2)
+                               (= (first i) :code))) i))
+             (:extra record)))
   (println)
 
   (let [syn (readline "syn")
@@ -39,9 +53,24 @@
     (reset! records (assoc-in @records (conj cursor :exc) exc))
 
     ; Do in a background thread.
+    ; TODO: Don't exit until results saved.
+    ; Is exc useful at all?
+    ; Review data.edn
+    ; Progress counter: how many items of total are done.
+    ; Are we doing L4 as well?
+    ; Document this. For instance seed in L4 has weight, but less than L6 rel and that is still less than L6 syn.
+    ; Write the algorythm (for the documentation) of how the matching will work.
     (future
       (save-results)
       (commit record))))
+
+(defn post-process [cursor record]
+  (when (not (vector? (get-in @records cursor)))
+    (reset! records (assoc-in @records (conj cursor :syn) (my-sorted-set (:syn record)))))
+  (when (not (vector? (get-in @records cursor)))
+    (reset! records (assoc-in @records (conj cursor :rel) (my-sorted-set (:rel record)))))
+  (when (not (vector? (get-in @records cursor)))
+    (reset! records (assoc-in @records (conj cursor :exc) (my-sorted-set (:exc record))))))
 
 (defn get-cursors [item cursor]
   (cond
@@ -63,7 +92,8 @@
               (if (and (vector? i)
                        (= (count i) 5))
                 (let [record (get-in @records i)]
-                  (when (empty? (apply concat (vals (select-keys record [:syn :rel :exc]))))
-                    (edit-record i record)))
+                  (if (empty? (apply concat (vals (select-keys record [:syn :rel :exc]))))
+                    (edit-record i record)
+                    (post-process i record)))
                 i))
             cursors))
